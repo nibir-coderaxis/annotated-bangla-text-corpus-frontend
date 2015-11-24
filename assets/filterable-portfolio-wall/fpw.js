bedf9226b92b5308cf34d529eea400a52af4a01c
tanvirshuvo <tanvir.hks@outlook.com>
2015-11-25 02:04:14 +0600

    Project base.

---
+++ assets/filterable-portfolio-wall/fpw.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $('.hidden').css('display','none');
+
+  $( "#filter button" ).each(function() {
+
+    $(this).on("click", function(){
+
+         var filter = $(this).attr('class');         
+
+      if ( $(this).attr('class') == 'all' ) {
+         $('.hidden').contents().appendTo('#posts').hide().show('slow');
+         $( "#filter button" ).removeClass('active');
+         $(this).addClass('active');
+         $("#filter button").attr("disabled", false);
+         $(this).attr("disabled", true);
+      }
+      else {
+         $('.post').appendTo('.hidden');
+         $('.hidden').contents().appendTo('#posts').hide().show('slow');
+         $('.post:not(.' + filter + ')').appendTo('.hidden').hide('slow');
+         $( "#filter button" ).removeClass('active');
+         $(this).addClass('active');
+         $("#filter button").attr("disabled", false);
+         $(this).attr("disabled", true);
+      };
+      
+      });
+
+  });
+
+});
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
